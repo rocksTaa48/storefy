ab3bcdc334c2794a1f6f9d1fe0a661b821aca072
rocksTaa <rockstaa48@yahoo.com>
2023-04-10 14:41:33 +0300

    cart_base_done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -22,4 +22,15 @@ class OrdersController < ApplicationController
     end
   end
 
+  def paid
+    @selected_order = Order.where(id: params[:order_ids]).destroy_all
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace('cart',
+                                                  partial: 'cart/cart',
+                                                  locals: {cart: @cart})
+      end
+    end
+  end
+
 end
